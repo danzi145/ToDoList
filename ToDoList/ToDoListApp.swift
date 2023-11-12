e5ce0a4ed67bc34bd7247d0069cc94b69e6ec792
danzi145 <codns145@gmail.com>
2023-11-13 00:54:25 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 박채운 on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
